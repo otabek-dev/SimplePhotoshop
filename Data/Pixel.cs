60748bd8a972c88969ff158bb40797feed711674
otabek231 <otabek.pro@hotmail.com>
2023-09-25 17:09:17 +0500

    Защита целостности - 1 сделано ✅

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -1,9 +1,44 @@
-﻿namespace MyPhotoshop
+﻿using System;
+
+namespace MyPhotoshop
 {
     public class Pixel
     {
-        public double R { get; set; } = 0;
-        public double G { get; set; } = 0;
-        public double B { get; set; } = 0;
+        private double r;
+        private double g;
+        private double b;
+        public double R
+        {
+            get => r;
+            set
+            {
+                if (value <= 1)
+                    r = value;
+                else
+                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
+            }
+        }
+        public double G
+        {
+            get => g;
+            set
+            {
+                if (value <= 1)
+                    g = value;
+                else
+                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
+            }
+        }
+        public double B
+        {
+            get => b;
+            set
+            {
+                if (value <= 1)
+                    b = value;
+                else
+                    throw new Exception(string.Format("Wrong channel value {0} (the value must be between 0 and 1", value));
+            }
+        }
     }
 }
