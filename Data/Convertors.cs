7ee5ae583e325dc932377879cf4ef9ad1df48e24
otabek231 <otabek.pro@hotmail.com>
2023-09-26 13:29:16 +0500

    Добавил индексатор в класс Photo

--- Data/Convertors.cs
+++ Data/Convertors.cs
@@ -13,9 +13,9 @@ namespace MyPhotoshop
                 for (int y = 0; y < bmp.Height; y++)
                 {
                     var pixel = bmp.GetPixel(x, y);
-                    photo.data[x, y].R = (double)pixel.R / 255;
-                    photo.data[x, y].G = (double)pixel.G / 255;
-                    photo.data[x, y].B = (double)pixel.B / 255;
+                    photo[x, y].R = (double)pixel.R / 255;
+                    photo[x, y].G = (double)pixel.G / 255;
+                    photo[x, y].B = (double)pixel.B / 255;
                 }
             return photo;
         }
@@ -33,9 +33,9 @@ namespace MyPhotoshop
             for (int x = 0; x < bmp.Width; x++)
                 for (int y = 0; y < bmp.Height; y++)
                     bmp.SetPixel(x, y, Color.FromArgb(
-                        ToChannel(photo.data[x, y].R),
-                        ToChannel(photo.data[x, y].G),
-                        ToChannel(photo.data[x, y].B)));
+                        ToChannel(photo[x, y].R),
+                        ToChannel(photo[x, y].G),
+                        ToChannel(photo[x, y].B)));
 
             return bmp;
         }
--- Data/Photo.cs
+++ Data/Photo.cs
@@ -4,7 +4,12 @@ namespace MyPhotoshop
     {
         public readonly int width;
         public readonly int height;
-        public readonly Pixel[,] data;
+        private readonly Pixel[,] data;
+
+        public Pixel this[int x, int y]
+        {
+            get => data[x, y];
+        }
 
         public Photo(int width, int height)
         {
